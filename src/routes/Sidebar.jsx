039e9c4a9679272dbde4a3bd1c422d55976ec219
George Balch <george.balch@proton.me>
2022-11-06 00:12:47 -0500

    Gridify sidebar link list

--- src/routes/Sidebar.jsx
+++ src/routes/Sidebar.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { 
     Link 
 } from "react-router-dom";
+import '../css/Sidebar.css';
 
 export default function Sidebar() {
 
     return (
-        <div>
+        <div className="link-list">
             <div><Link to='/'>Home</Link></div>
             <div><Link to='/transactions'>Transactions</Link></div>
             <div><Link to='/exchange'>Exchange</Link></div>
